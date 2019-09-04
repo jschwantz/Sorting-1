f3fc6853f74fee2199db5ea10c58691adc9df4e3
Jared Schwantz <c.jared.schwantz@gmail.com>
2019-09-04 12:34:46 -0400

    add comparator function

--- bubblesort.js
+++ bubblesort.js
@@ -1,14 +1,17 @@
 let swapCounter = 0;
 let comparisonCounter = 0;
 
+function comparator(itemA, itemB) {
+    return itemA>itemB
+}
 
-function bubbleSort(array) {
+function bubbleSort(array, compare=comparator) {
     let swapping = true;
 
     while (swapping) {
         swapping = false;
         for (let i =0; i<array.length-1; i++) {
-            if (array[i]>array[i+1]) {
+            if (compare(array[i], array[i+1])) {
                 swap(array, i, i+1);
                 swapping = true;
             }
